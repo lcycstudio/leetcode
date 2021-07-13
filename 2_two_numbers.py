00a769d856c9081475f2b431b7a1ad8f92bf1919
Ye Cheng Chen <lcyc29@gmail.com>
2021-07-13 00:32:07 -0700

    #2021

--- 2_two_numbers.py
+++ 2_two_numbers.py
@@ -24,31 +24,40 @@ Output: [8,9,9,9,0,0,0,1]
 
 
 
-def two_sum(nums, target):
-    if 2 > len(nums) or len(nums) > 1e4:
-        return "Array length must satisfy the condition: 2 <= length <= 1e4."
-    for each in nums:
-        if -1e9 > each or each > 1e9:
-            return "Each integer must satisfy the condition: -1e9 <= integer <= 1e4."
-    if -1e9 > target or target > 1e9:
-        return "Target must satisfy the condition: -1e9 <= target <= 1e9."
+def two_numbers(l1, l2):
+    # constraints
+    len_l1 = len(l1)
+    len_l2 = len(l2)
+    if len_l1 < 1 or len_l1 > 100 or len_l2 < 1 or len_l2 > 100:
+        return "The number of nodes in each linked list is in the range [1, 100]."
+    if sum(l1) > 9 * len_l1 or sum(l2) > 9 * len_l2:
+        return "0 <= Node.val <= 9"
+    if l1[0] == 0 or l2[0] == 0:
+        return "The list represents a number that does not have leading zeros."
     
-    for i in range(len(nums) - 1):
-        result = []
-        for j in range(1, len(nums)):
-            if nums[i] + nums[j] == target:
-                result.append(i)
-                result.append(j)
-                break
-            if len(result) != 0:
-                    break
-        if len(result) != 0:
-                break
+    new_l1 = []
+    new_l2 = []
+    for i in range(len_l1):
+        new_l1.append(l1[len_l1-1-i])
+    for i in range(len_l2):
+        new_l2.append(l2[len_l2-1-i])
+    str_l1 = ""
+    str_l2 = ""
+    for j in new_l1:
+        str_l1 += str(j)
+    for k in new_l2:
+        str_l2 += str(k)
+    
+    res_list = list(str(int(str_l1)+int(str_l2)))
+    res_len = len(res_list)
+    result = []
+    for l in range(res_len):
+        result.append(int(res_list[res_len-1-l]))
     return result
 
 
 if __name__ == "__main__":
-    nums = [-11**9]
-    target = -11**9
-    two_sum(nums, target)
-    print(two_sum(nums, target))
+    l1 = [9,9,9,9,9,9,9]
+    l2 = [9,9,9,9]
+    two_numbers(l1, l2)
+    print(two_numbers(l1, l2))
